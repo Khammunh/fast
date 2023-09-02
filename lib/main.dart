cba486b4245af445eaa1e7c1d3ccf2886690881a
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-09-02 22:42:58 +0700

    Work with long lists

--- lib/main.dart
+++ lib/main.dart
@@ -12,24 +12,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'again Grid List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        cardTheme: CardTheme(color: Colors.blue.shade50),
+        useMaterial3: true,
+        //Now, I learn here LayoutBuilder. Tomorrow I can try learn again Work with long lists
+      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Grid List'),
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: List.generate(
-            100,
-            (index) {
-              return Center(
-                child: Text(
-                  'Item $index',
-                  style: Theme.of(context).textTheme.headlineSmall,
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
                 ),
-              );
-            },
-          ),
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ItemWidget(text: 'Item 1'),
+                    ItemWidget(text: 'Item 2'),
+                    ItemWidget(text: 'Item 3'),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({super.key, required this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        height: 100,
+        child: Center(
+          child: Text(text),
         ),
       ),
     );
