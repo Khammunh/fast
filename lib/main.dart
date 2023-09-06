793bd5c6a69dbe19d5dc21a428629e8f8c07f21e
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-09-06 21:22:51 +0700

    learn building adaptive apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-
-const Color darkBlue = Color.Color.fromARGB(255, 18, 32, 47);
 
 void main() {
   runApp(
-    const MyApp(),
+    const MyApp(
+      
+    ),
   );
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
-      debugShowCheckedModeBanner: false,home: const Scaffold(
-        body: Center(
-          child: ,
-        ),
-      ),
-    );
+    return Container();
   }
 }
