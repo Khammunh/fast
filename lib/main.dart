4c4edf200b3021a1320ec305aa8f78e615503b5f
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-09-11 21:16:05 +0700

    Learn Design & Theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(
-    const MyApp(
-      
-    ),
+    const MyApp(),
   );
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    const title = 'Orientation Demo';
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Update app UI',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: OrientationBuilder(
+          builder: (context, orientation) {
+            return GridView.count(
+              crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+              children: List.generate(100, (index) {
+                return Center(
+                  child: Text(
+                    'Item $index',
+                    style: Theme.of(context).textTheme.displayLarge,
+                  ),
+                );
+              }),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
