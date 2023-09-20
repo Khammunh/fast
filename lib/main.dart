7b197eaae349bdb79980af210c5e66568e3ec7c2
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-09-20 12:55:15 +0700

    finsh handle

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'GestureDetector',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('GestureDetector'),
         ),
+        body: const Center(
+          child: MyButton(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        const snackBar = SnackBar(
+          content: Text('Tag message'),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(8),
+          
+        ),
+        child: const Text('My Button'),
       ),
     );
   }
