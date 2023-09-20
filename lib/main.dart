58a5c044174b2719f809b20caf627f058c3d05d7
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-09-20 12:10:30 +0700

    handle tag

--- lib/main.dart
+++ lib/main.dart
@@ -2,69 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const ParentWidget(),
+    const MyApp(),
   );
 }
 
-//Tapbox B
-
-class ParentWidget extends StatefulWidget {
-  const ParentWidget({super.key});
-
-  @override
-  State<ParentWidget> createState() => _ParentWidgetState();
-}
-
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: TapboxB(
-        onChanged: _handleTapboxChanged,
-        active: _active,
-      ),
-    );
-  }
-}
-
-class TapboxB extends StatelessWidget {
-  const TapboxB({
-    super.key,
-    this.active = false,
-    required this.onChanged,
-  });
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-  void _handleTap() {
-    onChanged(!active);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: Container(
-        width: 200,
-        height: 200,
-        decoration: BoxDecoration(
-          color: active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
-        child: Center(
-          child: Text(
-            active ? 'Active' : 'Inactive',
-            style: const TextStyle(
-              fontSize: 32,
-              color: Colors.white,
-            ),
-          ),
+    return MaterialApp(
+      title: 'GestureDetector',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('GestureDetector'),
         ),
       ),
     );
