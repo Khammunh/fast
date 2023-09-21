423221ebcaeaecc5026a4820cc9356f2355200ff
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-09-21 21:17:59 +0700

    learn Config

--- lib/main.dart
+++ lib/main.dart
@@ -2,63 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MyApp(),
+    MyHomePage(
+      item: List<String>.generate(10, (i) => 'item $i'),
+    ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    const title = 'InkWell Demo';
-    return const MaterialApp(
-      title: title,
-      home: MyHomePage(
-        title: title,
-      ),
-    );
-  }
 }
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({
     super.key,
-    required this.title,
+    required this.item,
   });
-  final String title;
-
+  final List<String> item;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: const Center(
-        child: MyInkWell(),
-      ),
-    );
-  }
-}
-
-class MyInkWell extends StatelessWidget {
-  const MyInkWell({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('My next Tag InkWell'),
-          ),
-        );
-      },
-      child: const Padding(
-        padding: EdgeInsets.all(23),
-        
-        child: Text('MyInkWell',style: TextStyle(color: Colors.cyan),),
-        
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('List Again'),
+        ),
+        body: ListView.builder(
+            itemCount: item.length,
+            prototypeItem: ListTile(
+              title: Text(
+                item.first,
+              ),
+            ),
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(item[index]),
+              );
+            }),
       ),
     );
   }
