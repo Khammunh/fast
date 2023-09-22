cbc722579634fda184ceb9a80e08dc4224acb9a3
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-09-22 17:08:43 +0700

    create and style a text field

--- lib/main.dart
+++ lib/main.dart
@@ -14,22 +14,40 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final items = List<String>.generate(20, (i) => 'Item ${i+1}');
+  final items = List<String>.generate(5, (i) => 'Item ${i + 1}');
   @override
   Widget build(BuildContext context) {
     const title = 'Data List';
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: title,
-      theme: ThemeData(
-        primarySwatch: Colors.green
-      ),
+      theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(title),
-        ),
-        body: ,
-      ),
+          appBar: AppBar(
+            title: const Text(title),
+          ),
+          body: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return Dismissible(
+                  key: Key(item),
+                  onDismissed: (direction) {
+                    setState(() {
+                      items.removeAt(index);
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('$item dismissed'),
+                      ),
+                    );
+                  },
+                  background: Container(color: Colors.red),
+                  child: ListTile(
+                    title: Text(item),
+                  ),
+                );
+              })),
     );
   }
 }
-
