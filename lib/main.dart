984f0c554995e17c3fba33461b51dd93a719ce6f
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-09-22 21:07:25 +0700

    Learn Text Form Field

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,48 +8,77 @@ void main() {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final items = List<String>.generate(5, (i) => 'Item ${i + 1}');
-  @override
   Widget build(BuildContext context) {
-    const title = 'Data List';
+    const titile = 'Retrieve Text Field';
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: title,
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text(title),
+      title: titile,
+      theme: ThemeData(primaryColor: Colors.cyan),
+      home: const MyHomePage(
+        title: titile,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final MyController = TextEditingController();
+  @override
+  void dispose() {
+    MyController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 8,
+              vertical: 10,
+            ),
+            child: TextField(
+              controller: MyController,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a search term'),
+            ),
           ),
-          body: ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
-                return Dismissible(
-                  key: Key(item),
-                  onDismissed: (direction) {
-                    setState(() {
-                      items.removeAt(index);
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('$item dismissed'),
-                      ),
-                    );
-                  },
-                  background: Container(color: Colors.red),
-                  child: ListTile(
-                    title: Text(item),
-                  ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: Text(MyController.text),
                 );
-              })),
+              });
+        },
+        tooltip: 'Show me the value!',
+        child: const Icon(
+          Icons.text_fields,
+        ),
+      ),
     );
   }
 }
