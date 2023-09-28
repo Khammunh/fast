9bfa3e84dc6086707436007e49f569b186b798e8
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-09-28 21:07:43 +0700

    Send data a new screen

---
+++ lib/class_todos.dart
@@ -0,0 +1,17 @@
+class Todo {
+  final String title;
+  final String description;
+
+  const Todo(
+    this.title,
+    this.description,
+  );
+}
+
+final todos = List.generate(
+  20,
+  (i) => Todo(
+    'Todo $i',
+    'A description of what needs to be done for Todo it',
+  ),
+);
