c763cf7ff2e7be41e2645d3a6d3658c71aae77c5
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-10-02 09:00:50 +0700

    return data

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,22 @@
-import 'package:fast/class_todos.dart';
-import 'package:fast/detailScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    title: 'Send data to a new screen',
-    home: FirstScreen(),
-  ));
+  runApp(
+    const HomeScreen(),
+  );
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const title = 'Send data';
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(title),
-      ),
-      body: ListView.builder(
-        itemCount: title.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(todos[index].title),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: ((context) => DetailScreen(todo: todos[index])),
-                ),
-              );
-            },
-          );
-        },
+    const title = 'Returning Dat Demo';
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
       ),
     );
   }
