f10ac1be212f54269024aaff17257c8761c501e3
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-10-03 23:26:53 +0700

    fix it

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,77 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text(title),
         ),
+        body: const Center(
+          child: SelectionButton(),
+        ),
+      ),
+    );
+  }
+}
+
+class SelectionButton extends StatefulWidget {
+  const SelectionButton({super.key});
+
+  @override
+  State<SelectionButton> createState() => _SelectionButtonState();
+}
+
+class _SelectionButtonState extends State<SelectionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: const Text('Pick on option, any option'),
+      onPressed: () {
+        _navigateAndDisplaySelection(context);
+      },
+    );
+  }
+}
+
+Future<void> _navigateAndDisplaySelection(BuildContext context) async {
+  final result = await Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => const SelectionScreen(),
+    ),
+  );
+  if (!mounted) return;
+  ScaffoldMessenger.of(context).removeCurrentSnackBar() .sho;
+}
+
+class SelectionScreen extends StatelessWidget {
+  const SelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick on option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, 'Yep!');
+                },
+                child: const Text('Yep!'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, 'Nope!');
+                },
+                child: const Text('Nope.'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
