24afb7f44f3ddece3ac03d6d91297e623e0517ae
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-10-04 22:19:04 +0700

    next learn again

--- lib/main.dart
+++ lib/main.dart
@@ -69,9 +69,33 @@ class _MyHomePageState extends State<MyHomePage> {
           padding: EdgeInsets.zero,
           children: [
             const DrawerHeader(
-              child: Text('Drawer Hearder'),
+              child: Text('Drawer Header'),
               decoration: BoxDecoration(color: Colors.blue),
             ),
+            ListTile(
+              title: const Text('Home'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                _onItemTapped(0);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Business'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                _onItemTapped(1);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('School'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                _onItemTapped(2);
+                Navigator.pop(context);
+              },
+            )
           ],
         ),
       ),
