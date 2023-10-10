1b6c39f4c62c45e0de6752ea98f2fb3cbac83679
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-10-10 12:05:29 +0700

    Badge package

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const SegmentedButtonApp(),
+    const MyApp(),
   );
 }
 
-class SegmentedButtonApp extends StatelessWidget {
-  const SegmentedButtonApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('SegmentedButtonApp'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Spacer(),
-              Text('Single choice')
-            ],
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _cartItemsNo = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // bottomNavigationBar: _bottomNavigationBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [_addRemoveCartButtons()],
       ),
     );
   }
+}
+
+// Widget _bottomNavigationBar() {
+//   BottomNavigationBar(items: )
+// }
+
+Widget _addRemoveCartButtons() {
+ return FloatingActionButton.extended(
+    onPressed: () {},
+    label: const Text('Add to cart'),
+    icon: const Icon(Icons.add),
+  );
 }
