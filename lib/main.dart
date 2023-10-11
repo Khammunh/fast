714170a9bd89b169824831828d220122f5f58cf9
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-10-12 06:49:57 +0700

    LinaearProgressIndicator

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,20 @@
-// ignore_for_file: non_constant_identifier_names, unused_element
-
 import 'package:flutter/material.dart';
+
+import 'progressIndicatorExample.dart';
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProgressIndicatorApp(),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ProgressIndicatorApp extends StatelessWidget {
+  const ProgressIndicatorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: const HomeScreen(),
+    return const MaterialApp(
+      home: ProgressIndicatorExample(),
     );
   }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-   int currentPageIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: _buttonNavigationBar(),
-      body: const Padding(
-        padding: EdgeInsets.all(2.0),
-        child: Row(
-          children: [Text('Home'),],
-        ),
-      ),
-    );
-  }
-}
-
-// Widget _bottomNavigationBar() {
-//   BottomNavigationBar(items: )
-// }
-Widget _buttonNavigationBar() {
-  return NavigationBar(
-    destinations: const [
-      NavigationDestination(
-        icon: Icon(Icons.home),
-        label: 'Home',
-      ),
-      NavigationDestination(
-        icon: Icon(Icons.chat),
-        label: 'Chat',
-      ),
-      NavigationDestination(
-        icon: Icon(Icons.chat),
-        label: 'Chat',
-      ),
-      
-    ],
-  animationDuration: Duration(ms:2000),
-  );
 }
---
+++ lib/progressIndicatorExample.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class ProgressIndicatorExample extends StatefulWidget {
+  const ProgressIndicatorExample({super.key});
+
+  @override
+  State<ProgressIndicatorExample> createState() =>
+      _ProgressIndicatorExampleState();
+}
+
+class _ProgressIndicatorExampleState extends State<ProgressIndicatorExample>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+        /// [AnimationController]s can be created with `vsync: this` because of
+      /// [TickerProviderStateMixin].
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+        controller.repeat(reverse: true);
+        super.initState();
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              'Linear progress indicator a fixed color',
+              style: TextStyle(fontSize: 20),
+            ),
+            LinearProgressIndicator(
+              value: controller.value,
+              semanticsLabel: 'Linear progress indicator',
+            ),
+          const  CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(Colors.green),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
