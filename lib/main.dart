4e669d16f27a807e63ba80831e9fa154c5648456
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-10-16 22:49:13 +0700

    finish Button Sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,19 @@
 import 'package:flutter/material.dart';
 
-import 'snackBarExample.dart';
+import 'buttonSheetExample.dart';
 
 void main() {
   runApp(
     const MyApp(),
   );
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("SnackBar"),
-        ),
-        body: const Center(
-          child: SnackBarExample(),
-        ),
-      ),
+    return const MaterialApp(
+      home: ButtonSheetExample(),
     );
   }
 }
