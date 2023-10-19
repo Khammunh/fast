c561278bc1abff602e65c334a009d4aec364701d
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-10-19 19:42:06 +0700

    learn list title

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
+import 'package:fast/listTitleExample.dart';
 import 'package:flutter/material.dart';
-
-import 'dividerExample.dart';
 
 void main() {
   runApp(
     const MyApp(),
   );
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title:const Text('Divider Example'),
+      theme: ThemeData(
+        listTileTheme: const ListTileThemeData(
+          textColor: Colors.white
         ),
-        body: const DividerExample(),
+        useMaterial3: true
       ),
+      home: const ListTitleExample(),
     );
   }
 }
