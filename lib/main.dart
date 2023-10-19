8a6bc89297b819e85666568ea9de4e29aa2a8095
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-10-19 22:44:15 +0700

    learn ListTitle move_avert

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:fast/listTitleExample.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,19 +5,47 @@ void main() {
     const MyApp(),
   );
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        listTileTheme: const ListTileThemeData(
-          textColor: Colors.white
-        ),
-        useMaterial3: true
-      ),
+      debugShowCheckedModeBanner: false,
       home: const ListTitleExample(),
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+    );
+  }
+}
+
+class ListTitleExample extends StatelessWidget {
+  const ListTitleExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListTitle Sample'),
+      ),
+      body: ListView(
+        children:const [
+          Card(
+            child: ListTile(
+              title: Text('One-line ListTitle'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: FlutterLogo(),
+              title: Text('One-line with leading widget'),
+            ),
+          ),
+          //Now tomorrow learn Card(more_vert) again.
+        ],
+      ),
     );
   }
 }
