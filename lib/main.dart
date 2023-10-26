c1fa9789f06ade0973a1f57c9efa3f27e801eca0
Khammunh <100525451+Khammunh@users.noreply.github.com>
2023-10-26 17:50:16 +0700

    NavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,171 +3,55 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const BottomAppBarDemo(),
+    const MyApp(),
   );
 }
 
-class BottomAppBarDemo extends StatefulWidget {
-  const BottomAppBarDemo({super.key});
-
-  @override
-  State<BottomAppBarDemo> createState() => _BottomAppBarDemoState();
-}
-
-class _BottomAppBarDemoState extends State<BottomAppBarDemo> {
-  bool _showFab = true;
-  bool _showNotch = true;
-  FloatingActionButtonLocation _fabLocation =
-      FloatingActionButtonLocation.endDocked;
-
-  void _onShowNotchChanged(bool value) {
-    setState(() {
-      _showNotch = value;
-    });
-  }
-
-  void _onShowFabChanged(bool value) {
-    setState(() {
-      _showFab = value;
-    });
-  }
-
-  void _onFabLocationChanged(FloatingActionButtonLocation? value) {
-    setState(() {
-      _fabLocation = value ?? FloatingActionButtonLocation.endDocked;
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    int currentPageIndex = 0;
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (BuildContext context) {
-                  return Scaffold(
-                    appBar: AppBar(
-                      title: const Text('Next AppBar'),
-                    ),
-                  );
-                }));
-              },
-              icon: const Icon(Icons.more_vert),
+          title: const Text('Navigation Bar'),
+        ),
+        body: const Center(
+          child: Text('Navigation Bar Example'),
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.explore),
+              label: 'Explore',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.settings),
+              label: 'Setting',
             ),
           ],
-          automaticallyImplyLeading: false,
-          title: const Text('Bottom App Bar Demo'),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.only(bottom: 88),
-          children: [
-            SwitchListTile(
-              title: const Text('Floating Action Button'),
-              value: _showFab,
-              onChanged: _onShowFabChanged,
-            ),
-            SwitchListTile(
-              title: const Text('Notch'),
-              value: _showNotch,
-              onChanged: _onShowNotchChanged,
-            ),
-            const Padding(
-              padding: EdgeInsets.all(16),
-            ),
-            RadioListTile(
-              value: FloatingActionButtonLocation.endDocked,
-              groupValue: _fabLocation,
-              onChanged: _onFabLocationChanged,
-              title: const Text('Docked - End'),
-            ),
-            RadioListTile(
-              value: FloatingActionButtonLocation.centerDocked,
-              groupValue: _fabLocation,
-              onChanged: _onFabLocationChanged,
-              title: const Text('Docked - Center'),
-            ),
-            RadioListTile(
-              value: FloatingActionButtonLocation.endFloat,
-              groupValue: _fabLocation,
-              onChanged: _onFabLocationChanged,
-              title: const Text('Floating - End'),
-            ),
-            RadioListTile(
-              value: FloatingActionButtonLocation.centerFloat,
-              groupValue: _fabLocation,
-              onChanged: _onFabLocationChanged,
-              title: const Text('Floating - Center'),
-            ),
-          ],
-        ),
-        floatingActionButton: _showFab
-            ? FloatingActionButton(
-                onPressed: () {},
-                tooltip: 'Create',
-                child: const Icon(Icons.add),
-              )
-            : null,
-        floatingActionButtonLocation: _fabLocation,
-        bottomNavigationBar: _DemoBottomAppBar(
-          fabLocation: _fabLocation,
-          shape: _showNotch ? const CircularNotchedRectangle() : null,
+          selectedIndex: currentPageIndex,
+          onDestinationSelected: (int index) {
+           
+          },
+          animationDuration:const Duration(microseconds: 1000),
+          backgroundColor: Colors.grey,
         ),
       ),
-    );
-  }
-}
-
-class _DemoBottomAppBar extends StatelessWidget {
-  const _DemoBottomAppBar({
-    super.key,
-    this.fabLocation = FloatingActionButtonLocation.endDocked,
-    this.shape = const CircularNotchedRectangle(),
-  });
-
-  final FloatingActionButtonLocation fabLocation;
-  final NotchedShape? shape;
-
-  static final List<FloatingActionButtonLocation> centerLocations =
-      <FloatingActionButtonLocation>[
-    FloatingActionButtonLocation.centerDocked,
-    FloatingActionButtonLocation.centerFloat,
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomAppBar(
-      shape: shape,
-      color: Colors.blue,
-      child: IconTheme(
-          data: IconThemeData(
-            color: Theme.of(context).colorScheme.onPrimary,
-          ),
-          child: Row(
-            children: [
-              IconButton(
-                tooltip: 'Open navigation menu',
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.menu,
-                ),
-              ),
-              if (centerLocations.contains(fabLocation)) const Spacer(),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.search),
-                tooltip: 'Search',
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.favorite),
-                tooltip: 'favorite',
-              ),
-            ],
-          )),
     );
   }
 }
